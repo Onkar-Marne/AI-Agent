8f3a11b99caf8bce67506365cea8dc5fc61dd683
Onkar Marne <onkar.marne22@gmail.com>
2025-08-20 11:41:42 +0530

    Updated

--- backend/src/api/ai/services.py
+++ backend/src/api/ai/services.py
@@ -2,7 +2,7 @@ from api.ai.llms import get_openai_llm
 from api.ai.schemas import EmailMessageSchema
 
 
-def generate_email_message(query:str):
+def generate_email_message(query:str) -> EmailMessageSchema:
     llm_base = get_openai_llm()
     llm = llm_base.with_structured_output(EmailMessageSchema)
     messages = [
--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,8 +1,9 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from api.db import get_session
-from api.ai.schemas import EmailMessageSchema
+from api.ai.agents import get_supervisor
+from api.ai.schemas import EmailMessageSchema, SupervisorMessageSchema
 from api.ai.services import generate_email_message
 from .models import ChatMessagePayLoad, ChatMessage, ChatMessageListItem
 
@@ -22,20 +23,37 @@ def chat_list_messages(session: Session = Depends(get_session)):
     return results
 
 
-# Invoke-WebRequest -Uri https://localhost:8080/api/chats/ -Method POST -Body '{"message":"Hello World"}' -ContentType "application/json"
+# Invoke-WebRequest -Uri http://localhost:8080/api/chats/ -Method POST -Body '{"message":"Hello World"}' -ContentType "application/json"
 
 # Invoke-WebRequest -Uri https://ai-agent-z625h.ondigitalocean.app/api/chats/ -Method POST -Body '{"message":"Hello World"}' -ContentType "application/json"
 
-# Invoke-WebRequest -Uri https://localhost:8080/api/chats/ -Method POST -Body '{"message":"Give me a summary of why its better to go out ..."}' -ContentType "application/json"
+# Invoke-WebRequest -Uri https://ai-agent-z625h.ondigitalocean.app/api/chats/ -Method POST -Body '{"message":"Give me a summary of why its better to go out"}' -ContentType "application/json"
+
+# Invoke-WebRequest -Uri http://localhost:8080/api/chats/ -Method POST -Body '{"message":"Give me a summary of why its better to go out"}' -ContentType "application/json"
 
 
-@router.post("/", response_model=EmailMessageSchema)
+@router.post("/", response_model=SupervisorMessageSchema)
 def chat_create_message(payload:ChatMessagePayLoad, session: Session = Depends(get_session)):
     data = payload.model_dump()
     print(data)
     obj = ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    #session.refresh(obj)
-    response = generate_email_message(payload.message)
-    return response
+    #response = generate_email_message(payload.message)
+    #return response
+    supe = get_supervisor()
+    msg_data = { 
+        "messages": [
+            {"role": "user",
+             "content": f"{payload.message}"
+            },
+        ]
+    }
+    result = supe.invoke(msg_data)
+    if not result:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    messages = result.get("messages")
+    if not messages:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    return messages[-1]
+
